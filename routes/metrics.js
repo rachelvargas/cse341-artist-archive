71ccbb7f3c062262472efe61568d1e31ae5c04de
Shaira <sil21019@byui.edu>
2023-01-31 22:01:48 +0000

    added metrics template routes

--- routes/metrics.js
+++ routes/metrics.js
@@ -3,8 +3,26 @@ const express = require('express');
 const router = express.Router();
 const metrics = require('../controllers/metrics');
 
+// GET ALL METRICS
 router.get('/', (req, res) => {
     res.send('Hey, this is the metrics.js route');
 });
+
+// CREATE NEW METRIC
+router.post('/', (req, res) => {
+    res.send('Hey, this is the metrics.js POST route');
+});
+
+// GET, PUT, DELETE BY ID ROUTES
+router.route('/:id')
+.get((req, res) => {
+    res.send('Hey, this is the metrics.js POST route');
+})
+.put((req, res) => {
+    res.send('Hey, this is the metrics.js PUT route');
+})
+.delete((req, res) => {
+    res.send('Hey, this is the metrics.js DELETE route');
+})
 
 module.exports = router;
