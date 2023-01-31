c7ae09a46a12a4b06817f62acdc16f6f1f39c57d
Shaira <sil21019@byui.edu>
2023-01-31 22:01:32 +0000

    added index.js routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
-const routes = require('express').Router();
+const express = require('express');
+const router = express.Router();
+const home = require('../controllers');
 
-routes.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Hey, this is the index.js route');
 });
 
-module.exports = routes
+router.use('/', require('./swagger.js'));
+router.use('/artists', require('./artists'))
+router.use('/showtimes', require('./showtimes'))
+router.use('/artworks', require('./artworks'))
+router.use('/metrics', require('./metrics'))
+router.use('/', home.displayHome)
+
+module.exports = router;
