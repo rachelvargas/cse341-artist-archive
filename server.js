d7eedd56d1fd40f401a71d7a474ef2b8b6436662
DanLeigh33 <vyv20001@byui.edu>
2023-02-01 13:17:42 +0200

    Updated server.js and connected database

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const conDB = require('./db/connect.js');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 
+
+conDB.connectDB();
 app.use('/', require('./routes'))
 
 app.listen(port, () => {
