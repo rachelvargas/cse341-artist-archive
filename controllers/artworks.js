e00e5bd9c0ae4604887fc75c28f687c9c05f8b05
DanLeigh33 <vyv20001@byui.edu>
2023-02-02 09:55:33 +0200

    Just connected one route to the DB for demonstration

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -1,1 +1,21 @@
-// Controller for the artworks collection
+const mongodb = require('../db/connect.js');
+//const ObjectId = require('mongodb').ObjectId;
+
+
+const getDB  = async (req, res) => {
+  try {
+    const allDB = await mongodb.getDatabase().db('artistarchive').collection('artworks').find();
+    allDB.toArray((err, dbs) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(dbs);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  };
+
+  module.exports = {getDB};
