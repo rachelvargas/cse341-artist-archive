d685bfb85b7a63650462a9c5cc7dc6e07bdb29f7
DanLeigh33 <vyv20001@byui.edu>
2023-02-03 12:08:30 +0200

    Revert "Merge pull request #15 from rachelvargas/dana"
    This reverts commit c666572e8b7491ea3d62b4afdf7c22c4336a8716, reversing
    changes made to 2db8a662a93d65dd13f678a0e424013cbf08b14a.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 
 conDB.connectDB();
-app.use('/', require('./routes'));
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
