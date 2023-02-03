a034ddb1aeea068d067e6eecfe04283dabc519c2
DanLeigh33 <vyv20001@byui.edu>
2023-02-03 11:32:13 +0200

    Update artworks.js

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -3,25 +3,14 @@ const mongodb = require('../db/connect.js');
 
 
 const getDB  = async (req, res) => {
-  try {
     console.log("@controller");
     const allDB = await mongodb.getDatabase().db('artistarchive').collection('artworks').find();
     console.log('@donemongodb');
-    allDB.toArray((err, dbs) => {
-      if (err) {
-        console.log("@controlleriferr");
-        res.status(400).json({ message: err });
-      }
-      console.log("@controllerelse200");
-      res.setHeader('Content-Type', 'application/json');
+    allDB.toArray().then((dbs) => {
+      console.log('@donemongodb2');
       res.status(200).json(dbs);
-      console.log("@controllerelse200");
+      console.log('@donemongodb3');
     });
-  } catch (err) {
-    console.log("@controllercatch");
-    res.status(500).json(err);
-    console.log("@controllercatch8");
-  }
 
   };
 
