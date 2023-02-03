6a7d84b2363f38f7a1eb9ede579628cdcc0c1c69
DanLeigh33 <vyv20001@byui.edu>
2023-02-03 11:38:36 +0200

    Corrected host, removed console logs

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -3,13 +3,10 @@ const mongodb = require('../db/connect.js');
 
 
 const getDB  = async (req, res) => {
-    console.log("@controller");
     const allDB = await mongodb.getDatabase().db('artistarchive').collection('artworks').find();
-    console.log('@donemongodb');
     allDB.toArray().then((dbs) => {
-      console.log('@donemongodb2');
       res.status(200).json(dbs);
-      console.log('@donemongodb3');
+      
     });
 
   };
