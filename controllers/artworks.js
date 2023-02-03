8b82899df6e2c7bb1ac1ff6cfae1467e80b33235
DanLeigh33 <vyv20001@byui.edu>
2023-02-03 12:15:13 +0200

    removed dv and eh

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -3,19 +3,12 @@ const mongodb = require('../db/connect.js');
 
 
 const getDB  = async (req, res) => {
-  try {
-    const allDB = await mongodb.getDatabase().db('artistarchive').collection('artworks').find();
-    allDB.toArray((err, dbs) => {
-      if (err) {
-        res.status(400).json({ message: err });
-      }
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(dbs);
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  const allDB = await mongodb.getDatabase().db('artistarchive').collection('artworks').find();
+  allDB.toArray().then((dbs) => {
+    res.status(200).json(dbs);
+    
+  });
 
-  };
+};
 
   module.exports = {getDB};
