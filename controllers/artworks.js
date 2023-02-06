08d6e865939498887a02231dc8cdd8d656907157
DanLeigh33 <vyv20001@byui.edu>
2023-02-06 11:55:47 +0200

    Showtimes routes setup

--- controllers/artworks.js
+++ controllers/artworks.js
@@ -3,15 +3,19 @@ const ObjectId = require('mongodb').ObjectId;
 
 
 const getDB  = async (req, res) => {
-  const allDB = await mongodb.getDatabase().db('artistarchive').collection('artworks').find();
-  allDB.toArray().then((dbs) => {
-    if (dbs){
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(dbs);
-    } else {
-      res.status(400).json(allDB.error || 'An error has occured');
-    }
-  });
+  try {
+    const allDB = await mongodb.getDatabase().db('artistarchive').collection('artworks').find();
+    allDB.toArray().then((dbs) => {
+      if (dbs){
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(dbs);
+      } else {
+        res.status(400).json(allDB.error || 'An error has occured');
+      }
+    });
+} catch (err) {
+  res.status(500).json(err);
+}
 
 };
 
