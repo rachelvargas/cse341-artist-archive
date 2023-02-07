ac708451fb1592d38510e664199385826f940174
Shaira <sil21019@byui.edu>
2023-02-07 11:17:53 +0000

    views files cleanup and text edits

--- controllers/index.js
+++ controllers/index.js
@@ -3,15 +3,10 @@ const app = express();
 app.set('view engine', 'ejs')
 
 displayHome = app.get('/', (req, res) => {
-    // const data =
-    //   'Portfolio App'
-
-    //GITHUB AUTH HOME
-    // res.render('home');
 
     // OIDC HOME
     res.send(req.oidc.isAuthenticated() ? res.render('home') : res.render('login'))
-    // res.send(req.oidc.isAuthenticated() ? res.render('login') : res.render('home'))
+
   });
 
 
