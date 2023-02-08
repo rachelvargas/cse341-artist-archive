6718279d82fd593ed95ab047ce933bb3b14202c7
Rachel Vargas <angeles.vargas@ymail.com>
2023-02-08 09:16:49 -0800

    I added the rest file,modified my metrics routes and controller, and pushing it to main brach

--- routes/metrics.js
+++ routes/metrics.js
@@ -5,46 +5,37 @@ const metricsController = require('../controllers/metrics');
 
 //const  { metricsValidation }  = require('./helpers/validation.js');
 
+router.get('/', metricsController.getMetrics);
+router.post('/', metricsController.newMetric);
+router.get('/:id', metricsController.getOne);
+router.put('/', metricsController.updateMetric);
+router.delete('/:id', metricsController.deleteMetric);
+
 
 // GET ALL METRICS
 ///router.get('/', metricsController.getMetrics);
 router.get('/', (metricsController, getMetrics) => {
-    res.send('Hey, this is the metrics.js GET route')
+    getMetrics.send('Hey, this is the metrics.js GET route')
 })
-
-//router.get('/', metricsController.getMetrics);
-
-
 
 // CREATE NEW METRIC
 router.post('/', (metricsController, newMetric ) => {
-    res.send('Hey, this is the metrics.js POST route');
+    newMetric.send('Hey, this is the metrics.js POST route');
 })
-
-//router.post('/', metricsController.newMetric);
-
 
 // GET, PUT, DELETE BY ID ROUTES
 router.route('/:id')
 .get((metricsController, getOne) => {
-    res.send('Hey, this is the metrics.js GET ONE BY ID route');
+    getOne.send('Hey, this is the metrics.js GET ONE BY ID route');
 })
-
-//router.get('/:id', metricsController.getOne);
-
 
 .put((metricsController, updateMetric) => {
-    res.send('Hey, this is the metrics.js PUT route');
+    updateMetric.send('Hey, this is the metrics.js PUT route');
 })
 
-//router.put('/', metricsController.updateMetric);
-
-
 .delete((metricsController, deleteMetric) => {
-    res.send('Hey, this is the metrics.js DELETE route');
-});
-
-//router.delete('/:id', metricController.deleteMetric);
+    deleteMetric.send('Hey, this is the metrics.js DELETE route');
+})
 
 
 module.exports = router;
