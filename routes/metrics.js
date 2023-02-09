41a0d0bc6616985d46a06ee26dcdd351d45f0515
baajayi <delly31@hotmail.com>
2023-02-09 20:21:28 +0100

    updated validation for the metric route

--- routes/metrics.js
+++ routes/metrics.js
@@ -4,14 +4,15 @@ const router = express.Router();
 const metricsController = require('../controllers/metrics');
 const validate = require('../middleware/validation')
 const { requiresAuth } = require('express-openid-connect');
+const validate = require('../middleware/validation');
 //const  { metricsValidation }  = require('./helpers/validation.js');
 
 // Auth0
 
 router.get('/', metricsController.getMetrics);
-router.post('/', requiresAuth(), metricsController.newMetric);
+router.post('/', [requiresAuth(), validate.saveMetric], metricsController.newMetric);
 router.get('/:id', metricsController.getOne);
-router.put('/:id', requiresAuth(), metricsController.updateMetric);
+router.put('/:id', [requiresAuth(), validate.saveMetric], metricsController.updateMetric);
 router.delete('/:id', requiresAuth(), metricsController.deleteMetric);
 
 
